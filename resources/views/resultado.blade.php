5723425a575f5c381c2eebde4d93dff80164cbd9
E-Vaga <evagasistema@gmail.com>
2018-11-05 03:02:14 -0200

    commit

--- resources/views/resultado.blade.php
+++ resources/views/resultado.blade.php
@@ -5,16 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Resultado</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
 <body>
-    MODELO: {{$veiculo->modelo}}<br>
-    ANO: {{$veiculo->ano}}<br>
-    COR: {{$veiculo->cor}}<br>
-    PLACA: {{$veiculo->placa}}<br>
-    CHASSI: {{$veiculo->chassi}}<br>
-    SITUACAO: {{$veiculo->situacao}}<br><br>
-    DATA CONSULTA: {{$veiculo->dataConsulta}}<br>
+    <br>
+    <div class=container>
+	    <div class="alert alert-primary" role="alert">
+	    MODELO: {{$veiculo->modelo}}<br>
+	    ANO: {{$veiculo->ano}}<br>
+	    COR: {{$veiculo->cor}}<br>
+	    PLACA: {{$veiculo->placa}}<br>
+	    CHASSI: {{$veiculo->chassi}}<br>
+	    SITUACAO: {{$veiculo->situacao}}<br><br>
+	    DATA CONSULTA: {{$veiculo->dataConsulta}}<br>
+	    </div>
+    </div>
 </body>
 </html>
